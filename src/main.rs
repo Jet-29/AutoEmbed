cc6041053a1984f91b023049e3c2cf2cdcbdb75d
Jet_29 <github@jetgames.space>
2024-04-06 00:17:46 +1100

    Added AutoStart host feature

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::process::Command;
 
+const HOST: &str = "Arachnid";
+const WAIT_TIME: u64 = 2;
+
 fn main() -> Result<(), &'static str> {
     let args = std::env::args();
     if args.len() > 1 {
@@ -28,25 +31,33 @@ fn main() -> Result<(), &'static str> {
 
     let chip = from_target_to_chip(target)?;
 
+    // Check Arachnid is online
+    if !ping_host() {
+        println!("Host not found attempting to start");
+        attempt_to_turn_on_host(10)?;
+    }
+
     // Begin commands
     // rsync
     Command::new("rsync")
         .args(["-a", "--delete"])
         .arg(format!("./../{current_dir}"))
-        .arg("Arachnid:embedded/")
+        .arg(format!("{HOST}:embedded/"))
         .spawn()
-        .or(Err("Failed to run rsync command"))?;
-    // TODO This could fail due to the Arachnid machine being offline
-    // I can make it check that first and power it on if not
+        .or(Err("Failed to run rsync command"))?
+        .wait()
+        .or(Err("Failed to wait for rsync"))?;
 
     // cargo-embed
     Command::new("ssh")
-        .args(["-t", "Arachnid"])
+        .args(["-t", HOST])
         .arg(format!(
             ". $HOME/.cargo/env; cd embedded/{current_dir}; cargo embed --chip {chip}"
         ))
         .spawn()
-        .or(Err("Failed to run embed command"))?;
+        .or(Err("Failed to run embed command"))?
+        .wait()
+        .or(Err("Failed to wait for ssh connection"))?;
 
     Ok(())
 }
@@ -56,4 +67,42 @@ fn from_target_to_chip(target: &str) -> Result<&'static str, &'static str> {
         "thumbv6m-none-eabi" => Ok("nRF51822_xxAA"),
         _ => Err("Target not recognised as a specific chip"),
     }
+}
+
+fn attempt_to_turn_on_host(attempts: u32) -> Result<(), &'static str> {
+    if !Command::new("/mnt/c/Program Files (x86)/VMware/VMware Workstation/vmrun.exe")
+        .args([
+            "start",
+            "c:\\Virtual Machines\\Arachnid\\Ubuntu.vmx",
+            "noGui",
+        ])
+        .output()
+        .unwrap()
+        .status
+        .success()
+    {
+        return Err("Failed to launch VM");
+    }
+
+    for attempt in 0..attempts {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        if ping_host() {
+            println!("Ping attempt: {attempt} [ SUCCESS ]");
+            println!("Waiting {WAIT_TIME} seconds for system to fully start");
+            std::thread::sleep(std::time::Duration::from_secs(WAIT_TIME));
+            return Ok(());
+        }
+        println!("Ping attempt: {attempt} [ FAIL ]");
+    }
+
+    Err("Failed to get a response from Arachnid")
+}
+
+fn ping_host() -> bool {
+    Command::new("ping")
+        .args(["-c 1", HOST])
+        .output()
+        .unwrap()
+        .status
+        .success()
 }
