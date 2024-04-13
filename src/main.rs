7ce1c16495925a779ecd05eb5016ab70f39c7830
Jet_29 <github@jetgames.space>
2024-04-14 01:19:57 +1000

    Updated to use new flash program

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,48 @@
-use std::{fmt::Debug, process::Command};
+use std::process::Command;
 
 const HOST: &str = "Arachnid";
 const WAIT_TIME: u64 = 2;
 
+const EMBEDDED_DIR: &str = "dev/rust/Microbit";
+
 fn main() -> Result<(), &'static str> {
-    let args = std::env::args();
-    if args.len() > 1 {
-        // TODO Added features for running other probe-rs commands
-        return Err("Extra args are not supported yet");
+    // Get project to embed.
+    let mut args = std::env::args();
+    args.next(); // Skip the first arg.
+    let project_name = args.next().ok_or("Specify the project")?;
+
+    let home_dir = std::env::var("HOME").or(Err("No HOME env var"))?;
+    let home_path = std::path::Path::new(&home_dir);
+    let embedded_path = home_path.join(EMBEDDED_DIR);
+    let project_path = embedded_path.join("projects").join(&project_name);
+
+    // Check the project folder is valid
+    if !project_path.exists() {
+        return Err("Project specified can not be found");
     }
 
-    let full_path = std::env::current_dir().unwrap();
-    let current_dir = full_path.iter().last().unwrap().to_str().unwrap();
-
-    if !std::path::Path::new("Embed.toml").exists() {
-        return Err("Not in an valid project, Please add an Embed.toml file");
-    }
-
-    // Check Arachnid is online
+    // Check HOST is online
     if !ping_host() {
         println!("Host not found attempting to start");
         attempt_to_turn_on_host(10)?;
     }
 
     // Begin commands
-    // rsync
+    // Sync the embedded workspace
     Command::new("rsync")
         .args(["-a", "--delete"])
-        .arg(format!("./../{current_dir}"))
+        .arg(embedded_path.into_os_string())
         .arg(format!("{HOST}:embedded/"))
         .spawn()
         .or(Err("Failed to run rsync command"))?
         .wait()
         .or(Err("Failed to wait for rsync"))?;
 
-    // cargo-embed
+    // probe_flash
     Command::new("ssh")
         .args(["-t", HOST])
         .arg(format!(
-            ". $HOME/.cargo/env; cd embedded/{current_dir}; cargo embed"
+            ". $HOME/.cargo/env; cd embedded/Microbit/projects/{project_name}; ~/embedded/probe_embed"
         ))
         .spawn()
         .or(Err("Failed to run embed command"))?
@@ -48,6 +52,7 @@ fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
+// Attempts to run the vmware start command for my machine.
 fn attempt_to_turn_on_host(attempts: u32) -> Result<(), &'static str> {
     if !Command::new("/mnt/c/Program Files (x86)/VMware/VMware Workstation/vmrun.exe")
         .args([
@@ -77,6 +82,7 @@ fn attempt_to_turn_on_host(attempts: u32) -> Result<(), &'static str> {
     Err("Failed to get a response from Arachnid")
 }
 
+// Pings the host ones and returns if it was successful
 fn ping_host() -> bool {
     Command::new("ping")
         .args(["-c 1", HOST])
